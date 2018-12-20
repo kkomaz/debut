d27ba312456e00d5a122a0fcb2c8af96116eae00
Alex Lee <alex@uniteus.com>
2018-12-19 19:01:10 -0500

    Login page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,40 @@
 import React, { Component } from 'react'
+import { UserSession } from 'blockstack'
+import { Button } from 'react-bulma-components'
+import { appConfig } from 'utils/constants'
+import Login from 'components/Login'
 import 'stylesheets/main.scss'
 
 class App extends Component {
+  state = {
+    userSession: new UserSession({ appConfig })
+  }
+
+  componentDidMount = async () => {
+    const { userSession } = this.state
+
+    if (!userSession.isUserSignedIn() && userSession.isSignInPending()) {
+      const userData = await userSession.handlePendingSignIn()
+
+      if (!userData.username) {
+        throw new Error('This app requires a username')
+      }
+      window.location = '/'
+    }
+  }
+
   render() {
+    const { userSession } = this.state
+
+    console.log(userSession.isUserSignedIn())
+
     return (
       <div className="App">
-        Hello World
+        {
+          userSession.isUserSignedIn() ?
+          <div><Button onClick={() => userSession.signUserOut()}>Sign Out</Button></div> :
+          <Login userSession={userSession} />
+        }
       </div>
     );
   }
---
+++ src/components/Login/index.jsx
@@ -0,0 +1,49 @@
+import React, { Component } from 'react'
+import {
+  Button,
+  Columns,
+} from 'react-bulma-components'
+import debut from 'images/debut.png'
+import "./_login.scss"
+
+class Login extends Component {
+  state = {
+    loadingUser: false,
+  }
+
+  signIn = (e) => {
+    const { userSession } = this.props
+
+    e.preventDefault()
+    userSession.redirectToSignIn()
+    this.setState({ loadingUser: true })
+  }
+
+  render() {
+    const { loadingUser } = this.state;
+
+    return (
+      <Columns className="login">
+        <Columns.Column className="login-column-left" size={6}>
+        </Columns.Column>
+
+        <Columns.Column className="login-column-right" size={6}>
+          {
+            loadingUser ? <div>Loading...</div> :
+            <div className="login-blockstack">
+              <img src={debut} alt="Logo" />
+              <Button
+                color="primary"
+                onClick={this.signIn}
+                >
+                Sign in with Blockstack
+              </Button>
+            </div>
+          }
+        </Columns.Column>
+      </Columns>
+    );
+  }
+}
+
+export default Login;
