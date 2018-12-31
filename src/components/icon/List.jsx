68d285e7461a933e0b04865818517b335d277e36
Alex Lee <alex@uniteus.com>
2018-12-30 19:06:25 -0500

    Add default src

--- src/components/icon/List.jsx
+++ src/components/icon/List.jsx
@@ -1,23 +1,30 @@
-import React from 'react'
+import React, { Component } from 'react'
 import _ from 'lodash'
 import 'stylesheets/components/icon/icon-list.scss'
 
-const IconList = ({ apps }) => {
-  return (
-    <ul className="icon-list">
-      {
-        _.map(apps, (app) => {
-          return (
-            <li className="icon-list__single">
-              <a href={app.url} target="_blank" rel='noreferrer noopener'>
-                <img src={app.icons[0].src} alt="dapp" height="42" width="42" />
-              </a>
-            </li>
-          )
-        })
-      }
-    </ul>
-  )
+class IconList extends Component {
+  addDefaultSrc = (evt) => {
+    evt.target.src = 'https://i.imgur.com/w1ur3Lq.jpg'
+  }
+
+  render() {
+    const { apps } = this.props
+    return (
+      <ul className="icon-list">
+        {
+          _.map(apps, (app, index) => {
+            return (
+              <li className="icon-list__single" key={`${app}-${index}`}>
+                <a href={app.url} target="_blank" rel='noreferrer noopener'>
+                  <img onError={this.addDefaultSrc} src={app.icons[0].src} alt="dapp" height="42" width="42" />
+                </a>
+              </li>
+            )
+          })
+        }
+      </ul>
+    )
+  }
 }
 
 export default IconList
