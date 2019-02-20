f415648a044b29851ddbc49c555f388ab8c9fb3d
Alex Lee <alex@uniteus.com>
2019-02-20 00:21:32 -0500

    description loader clean-up

--- src/components/User/UserDescription.jsx
+++ src/components/User/UserDescription.jsx
@@ -2,13 +2,10 @@ import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import {
   Button,
-  Card,
-  Content
 } from 'react-bulma-components'
 import UserIntroDisplay from 'components/User/IntroDisplay'
 import UserIntroForm from 'components/User/UserIntroForm'
 import { List } from 'react-content-loader'
-import { Loadable } from 'components/Loader'
 
 class UserDescription extends Component {
   static propTypes = {
@@ -34,60 +31,46 @@ class UserDescription extends Component {
 
     if (!adminMode) {
       return (
-        <Card className="user-description">
-          <Card.Content>
-            <Content>
-              <h4>About Myself</h4>
-              {
-                loading ? <List /> :
-                <UserIntroDisplay
-                  adminMode={adminMode}
-                  description={userInfo.description}
-                  />
-              }
-            </Content>
-          </Card.Content>
-        </Card>
+        <div className="user-description">
+          <h4>About Myself</h4>
+          {
+            loading ? <List /> :
+            <UserIntroDisplay
+              adminMode={adminMode}
+              description={userInfo.description}
+              />
+          }
+        </div>
       )
     }
 
-    console.log(fileExists)
-
     return (
-      <Card className="user-description">
-        <Card.Content>
-          <Content>
-            <h4>About Myself</h4>
-            <Loadable loading={loading}>
-              <div className="user-description__info-details">
-                <div className="user-description__button-actions mb-one">
-                  {
-                    fileExists ?
-                    <Button onClick={this.props.onCreateEdit} color="primary" className="mr-half">
-                      Edit
-                    </Button> :
-                    <Button onClick={this.props.onCreateEdit} color="primary" className="mr-half">
-                      Create
-                    </Button>
-                  }
-                </div>
-                {
-                  displayView ? <UserIntroDisplay description={userInfo.description} /> :
-                  <UserIntroForm
-                    description={userInfo.description}
-                    fileExists={fileExists}
-                    onCancel={this.props.onCancel}
-                    onSubmit={this.props.onSubmit}
-                    identityAddress={sessionUser.userData.identityAddress}
-                    userSession={sessionUser.userSession}
-                    username={username}
-                    />
-                }
-              </div>
-            </Loadable>
-          </Content>
-        </Card.Content>
-      </Card>
+      <div className="user-description__info-details">
+        <h4>About Myself</h4>
+        <div className="user-description__button-actions mb-one">
+          {
+            fileExists ?
+            <Button onClick={this.props.onCreateEdit} color="primary" className="mr-half">
+              Edit
+            </Button> :
+            <Button onClick={this.props.onCreateEdit} color="primary" className="mr-half">
+              Create
+            </Button>
+          }
+        </div>
+        {
+          displayView ? <UserIntroDisplay description={userInfo.description} /> :
+          <UserIntroForm
+            description={userInfo.description}
+            fileExists={fileExists}
+            onCancel={this.props.onCancel}
+            onSubmit={this.props.onSubmit}
+            identityAddress={sessionUser.userData.identityAddress}
+            userSession={sessionUser.userSession}
+            username={username}
+            />
+        }
+      </div>
     )
   }
 }
