360e61866656f000028659d50162c916a49d87ce
Alex Lee <alex@uniteus.com>
2019-03-02 09:57:44 -0500

    fix logic for unsigned and current user

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -2,3 +2,4 @@ import { AppConfig } from 'blockstack'
 
 export const appConfig = new AppConfig(['store_write', 'publish_data'])
 export const POST_FILENAME = 'info.json'
+export const nodeEnv = process.env.NODE_ENV === 'development' ? 'development' : 'production'
