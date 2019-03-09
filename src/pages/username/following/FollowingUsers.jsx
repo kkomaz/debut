789bc7ec56e8c98444e25365b0a3322cff0df406
Alex Lee <alex@uniteus.com>
2019-03-09 16:58:39 -0500

    Update following pagination

--- src/pages/username/following/FollowingUsers.jsx
+++ src/pages/username/following/FollowingUsers.jsx
@@ -6,15 +6,24 @@ import {
   Container,
   Card,
 } from 'components/bulma'
+import { BarLoader } from 'components/Loader'
 import PropTypes from 'prop-types'
 import { User } from 'radiks'
 import _ from 'lodash'
 import { UserContext } from 'components/User/UserProvider'
 
 class FollowingUsers extends Component {
-  state = {
-    offset: 0,
-    users: [],
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      offset: 0,
+      users: [],
+      bottomReached: false,
+      full: false,
+    }
+
+    this.handleScroll = _.debounce(this.handleScroll, 300)
   }
 
   static propTypes = {
@@ -23,14 +32,19 @@ class FollowingUsers extends Component {
 
   componentDidMount = async () => {
     this.fetchUsers()
+    window.addEventListener('scroll', this.handleScroll)
   }
 
-  componentDidUpdate(prevProps, prevState) {
+  componentDidUpdate = async (prevProps, prevState) => {
     if (this.props.follow.username !== prevProps.follow.username) {
       this.setState({ users: [], offset: 0 }, () => {
         this.fetchUsers()
       })
     }
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('scroll', this.handleScroll)
   }
 
   fetchUsers = async () => {
@@ -41,13 +55,20 @@ class FollowingUsers extends Component {
       const result = await User.fetchList({
         username: follow.following,
         sort: '-createdAt',
-        limit: 10,
+        limit: 8,
         offset,
       })
 
-      this.setState({
-        users: _.map(result, 'attrs'),
-        offset: result.length
+      if (_.isEmpty(result)) {
+        return this.setState({ full: true })
+      }
+
+      const additionalUsers = _.map(result, 'attrs')
+      const finalUsers = [...this.state.users, ...additionalUsers]
+
+      return this.setState({
+        users: finalUsers,
+        offset: finalUsers.length
       })
     }
   }
@@ -57,8 +78,32 @@ class FollowingUsers extends Component {
     history.push(`/${user.username}`)
   }
 
+  handleScroll = () => {
+    const { bottomReached } = this.state
+    const html = document.documentElement; // get the html element
+    // window.innerHeight - Height (in pixels) of the browser window viewport including, if rendered, the horizontal scrollbar.
+    // html.offsetHeight - read-only property returns the height of an element, including vertical padding and borders, as an integer.
+    const windowHeight = "innerHeight" in window ? window.innerHeight : html.offsetHeight;
+    const body = document.body; // get the document body
+    const docHeight = Math.max(body.scrollHeight, body.offsetHeight, html.clientHeight, html.scrollHeight, html.offsetHeight); // Find the max value of the overall doc
+    const windowBottom = windowHeight + window.pageYOffset; // Viewport + height offset post scroll
+
+    /**
+     * if windowBottom is larger then you know you reached the bottom
+    */
+    if (windowBottom >= docHeight) {
+      this.setState({ bottomReached: true }, () => {
+        if (!this.state.full) {
+          this.fetchUsers()
+        }
+      });
+    } else if ((windowBottom < docHeight) && bottomReached) {
+      this.setState({ bottomReached: false });
+    }
+  }
+
   render() {
-    const { users } = this.state
+    const { users, bottomReached, full } = this.state
     const { defaultImgUrl } = this.context.state
     const { className } = this.props
 
@@ -83,6 +128,9 @@ class FollowingUsers extends Component {
                 </Columns.Column>
               )
             })
+          }
+          {
+            bottomReached && !full && <BarLoader style={{ height: '200px' }} />
           }
         </Columns>
       </Container>
