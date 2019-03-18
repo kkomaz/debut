df7d7d3ad757f3d527bc0780a48716d36db1279c
Alex Lee <alex@uniteus.com>
2019-03-17 21:03:20 -0400

    css clean-up

--- src/routes/AdminUsernameRoute.jsx
+++ src/routes/AdminUsernameRoute.jsx
@@ -18,10 +18,30 @@ import BarLoader from 'components/Loader/BarLoader'
 import './AdminMenu.scss'
 
 class AdminUsernameRoute extends Component {
+  state = {
+    active: 'activityFeed'
+  }
+
   static propTypes = {
     match: PropTypes.object.isRequired,
     username: PropTypes.string.isRequired,
     userFollow: PropTypes.object.isRequired,
+  }
+
+  onMenuItemClick = (value) => {
+    this.setState({ active: value })
+  }
+
+  onActivityFeedClick = () => {
+    this.onMenuItemClick('activityFeed')
+  }
+
+  onFollowersClick = () => {
+    this.onMenuItemClick('followers')
+  }
+
+  onFollowingClick = () => {
+    this.onMenuItemClick('following')
   }
 
   render() {
@@ -31,15 +51,17 @@ class AdminUsernameRoute extends Component {
       userFollow,
     } = this.props
 
+    const { active } = this.state
+
     return (
       <Container>
         <Columns>
           <Columns.Column size={2}>
             <Menu className="admin-menu">
-              <Menu.List title={username}>
-                <Menu.List.Item>Activity Feed</Menu.List.Item>
-                <Menu.List.Item>Following</Menu.List.Item>
-                <Menu.List.Item>Followers</Menu.List.Item>
+              <Menu.List title={username} className="admin-menu__title">
+                <Menu.List.Item active={active === 'activityFeed'} onClick={this.onActivityFeedClick}>Activity Feed</Menu.List.Item>
+                <Menu.List.Item active={active === 'followers'} onClick={this.onFollowersClick}>Following</Menu.List.Item>
+                <Menu.List.Item active={active === 'following'} onClick={this.onFollowingClick}>Followers</Menu.List.Item>
               </Menu.List>
             </Menu>
           </Columns.Column>
