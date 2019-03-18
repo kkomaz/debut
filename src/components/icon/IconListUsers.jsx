c1906ae1b5995df5c58078cc263fb5d61fabeb95
Alex Lee <alex@uniteus.com>
2019-03-18 17:46:10 -0400

    Transitions

--- src/components/icon/IconListUsers.jsx
+++ src/components/icon/IconListUsers.jsx
@@ -3,7 +3,7 @@ import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import { withRouter, Link } from 'react-router-dom'
 import _ from 'lodash'
-import { CSSTransitionGroup } from 'react-transition-group'
+import ReactCSSTransitionReplace from 'react-css-transition-replace';
 
 // Component Imports
 import { UserContext } from 'components/User/UserProvider'
@@ -14,6 +14,10 @@ import './IconListUsers.scss'
 class IconListUsers extends Component {
   static propTypes = {
     history: PropTypes.object.isRequired
+  }
+
+  componentWillUnmount() {
+    console.log('unmounting')
   }
 
   addDefaultSrc = (evt) => {
@@ -30,34 +34,37 @@ class IconListUsers extends Component {
     const { defaultImgUrl } = this.context.state
 
     return (
-        <ul className="icon-list-users">
-          <CSSTransitionGroup
-            transitionName="icon-list-users-transition"
-            transitionEnterTimeout={500}
-            transitionLeaveTimeout={300}
-          >
-          {
-            _.map(users, (user, index) => {
-              return (
-                <li className="icon-list-users__single" key={`${user.username}-${index}`}>
-                  <Link className="icon-list-users__single-link" to={`/user/${user.username}`}>
-                    <img
-                      onError={this.addDefaultSrc}
-                      src={_.get(user, 'profileImgUrl', defaultImgUrl)}
-                      alt="user"
-                      height="42"
-                      width="42"
-                    />
-                  </Link>
-                  <p className="icon-list-users__username ml-one mb-half">
-                    {user.username}
-                  </p>
-                </li>
-              )
-            })
-          }
-          </CSSTransitionGroup>
-        </ul>
+      <ReactCSSTransitionReplace
+        transitionName="cross-fade"
+        transitionEnterTimeout={1000}
+        transitionLeaveTimeout={100}
+      >
+        {
+          _.isEmpty(users) ? null :
+          <ul className="icon-list-users" key="icon-list-users">
+            {
+              _.map(users, (user, index) => {
+                return (
+                  <li className="icon-list-users__single" key={`${user.username}-${index}`}>
+                    <Link className="icon-list-users__single-link" to={`/user/${user.username}`}>
+                      <img
+                        onError={this.addDefaultSrc}
+                        src={_.get(user, 'profileImgUrl', defaultImgUrl)}
+                        alt="user"
+                        height="42"
+                        width="42"
+                        />
+                    </Link>
+                    <p className="icon-list-users__username ml-one mb-half">
+                      {user.username}
+                    </p>
+                  </li>
+                )
+              })
+            }
+          </ul>
+        }
+      </ReactCSSTransitionReplace>
     )
   }
 }
