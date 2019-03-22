5a538825772e4f3eeb809225f73d00ed4d17f9d5
Alex Lee <alex@uniteus.com>
2019-03-22 17:53:27 -0400

    Tagging

--- src/utils/decorator/linkifyText.js
+++ src/utils/decorator/linkifyText.js
@@ -5,7 +5,7 @@ const processString = require('react-process-string')
 /* eslint-disable */
 let config = [
   {
-    regex: /@[a-z0-9]+\.[a-z]*\.?[a-z]*/gim,
+    regex: /@[a-z0-9]+\.+[a-z]+\.?[a-z]+/gim,
     fn: (key, result) => {
       const user = result[0].substring(1).trim()
       return (
@@ -17,15 +17,31 @@ let config = [
   },
   {
     regex: /(http|https):\/\/(\S+)\.([a-z]{2,}?)(.*?)( |\,|$|\.)/gim,
-    fn: (key, result) => <span key={key}>
-                             <a target="_blank" rel="noopener noreferrer" href={`${result[1]}://${result[2]}.${result[3]}${result[4]}`}>{result[2]}.{result[3]}{result[4]}</a>{result[5]}
-                         </span>
+    fn: (key, result) => {
+      console.log('hitting middle')
+      return (
+        <span key={key}>
+          <a target="_blank" rel="noopener noreferrer" href={`${result[1]}://${result[2]}.${result[3]}${result[4]}`}>{result[2]}.{result[3]}{result[4]}</a>{result[5]}
+        </span>
+      )
+    }
   },
   {
     regex: /(\S+)\.([a-z]{2,}?)(.*?)( |\,|$|\.)/gim,
-    fn: (key, result) => <span key={key}>
-                             <a target="_blank" rel="noopener noreferrer" href={`http://${result[1]}.${result[2]}${result[3]}`}>{result[1]}.{result[2]}{result[3]}</a>{result[4]}
-                         </span>
+    fn: (key, result) => {
+      if (result[0][0] === '#') {
+        return (
+          <span key={key}>
+            {result[0]}
+          </span>
+        )
+      }
+      return (
+        <span key={key}>
+          <a target="_blank" rel="noopener noreferrer" href={`http://${result[1]}.${result[2]}${result[3]}`}>{result[1]}.{result[2]}{result[3]}</a>{result[4]}
+        </span>
+      )
+    }
   }];
 
 const linkifyText = (text) => {
