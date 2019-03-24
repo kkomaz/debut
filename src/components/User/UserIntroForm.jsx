a6dd34b25d8ae760bd84c06fd18503959aaeb0a5
Alex Lee <alex@uniteus.com>
2019-03-24 12:02:21 -0400

    searchable-remove-keys

--- src/components/User/UserIntroForm.jsx
+++ src/components/User/UserIntroForm.jsx
@@ -10,9 +10,11 @@ import {
 } from 'react-bulma-components/lib/components/form'
 import {
   Input,
+  Dropdown,
 } from 'components/bulma'
 import axios from 'axios'
 import SubmitFooter from 'components/UI/Form/SubmitFooter'
+import './UserIntroForm.scss'
 
 class UserIntroForm extends Component {
   constructor(props) {
@@ -23,9 +25,12 @@ class UserIntroForm extends Component {
     this.state = {
       description,
       valid: true,
-      city: props.city || '',
+      area: props.area || '',
       websiteUrl: props.websiteUrl || '',
       name: props.name || '',
+      searchResults: [],
+      hovered: '',
+      open: false,
     }
 
     this.fetchCityList = _.debounce(this.fetchCityList, 1000)
@@ -35,6 +40,16 @@ class UserIntroForm extends Component {
     description: PropTypes.string,
     onCancel: PropTypes.func,
     onSubmit: PropTypes.func,
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (this.dropdown.state.open) {
+      document.addEventListener('keydown', this.onKeyPress, false)
+    }
+
+    if (!this.dropdown.state.open) {
+      document.removeEventListener('keydown', this.onKeyPress)
+    }
   }
 
   onChange = (e) => {
@@ -58,8 +73,13 @@ class UserIntroForm extends Component {
 
     const target = e.target.value
 
+    if (!this.dropdown.state.open) {
+      this.dropdown.toggle()
+    }
+
     this.setState({
-      [e.target.name]: e.target.value
+      [e.target.name]: e.target.value,
+      open: true,
     }, () => {
       this.fetchCityList(target)
     })
@@ -97,62 +117,147 @@ class UserIntroForm extends Component {
   }
 
   fetchCityList = async (value) => {
-    const result = await axios.get('http://autocomplete.geocoder.api.here.com/6.2/suggest.json', {
+    const response = await axios.get('http://autocomplete.geocoder.api.here.com/6.2/suggest.json', {
       params: {
         app_id: 'HcfQzK5jZbS7VenQDIPz',
         app_code: '3T1_Gjk5H-BCkrK3QtraUg',
-        query: value
+        query: value,
+        resultType: 'city'
       }
     })
-    console.log(result)
+
+    const result = _.reduce(response.data.suggestions, (acc, curr) => {
+      if (curr.address.city) {
+        if (_.includes(acc, `${curr.address.city}, ${curr.address.country}`)) {
+          return [...acc]
+        }
+
+        return [...acc, `${curr.address.city}, ${curr.address.country}`]
+      }
+
+      return [...acc]
+    }, [])
+    return this.setState({ searchResults: result })
+  }
+
+  onDropdownChange = (selected) => {
+    this.setState({
+      area: selected,
+      selected,
+    })
+  }
+
+  onKeyPress = (e) => {
+    const { hovered } = this.state
+    if (this.dropdown.state.open && e.keyCode === 13) {
+      if (this.state.hovered) {
+        this.setState({
+          area: hovered,
+          selected: hovered,
+          open: false
+        }, () => {
+          this.dropdown.toggle(e)
+        })
+      }
+    }
+
+    // Arrow up
+    if (this.dropdown.state.open && e.keyCode === 38) {
+      const index = _.findIndex(this.state.searchResults, (username) => {
+        return username === this.state.hovered
+      })
+
+      return this.setState({ hovered: this.state.searchResults[index - 1]})
+    }
+
+    // Arrow down
+    if (this.dropdown.state.open && e.keyCode === 40) {
+      if (this.state.hovered === '') {
+        return this.setState({ hovered: this.state.searchResults[0]})
+      } else {
+        const index = _.findIndex(this.state.searchResults, (username) => {
+          return username === this.state.hovered
+        })
+
+        if (index === this.state.searchResults.length - 1) {
+          return this.setState({ hovered: this.state.searchResults[index]})
+        } else {
+          return this.setState({ hovered: this.state.searchResults[index + 1]})
+        }
+      }
+    }
   }
 
   render() {
-    const { valid } = this.state
+    const { valid, searchResults } = this.state
 
     return (
-      <form className="user-intro-form" onSubmit={this.onSubmit}>
-        <Field>
-          <Input
-            className="mb-half"
-            name="name"
-            onChange={this.onChange}
-            placeholder="Name"
-            value={this.state.name}
-          />
+      <React.Fragment>
+        <form className="user-intro-form" onSubmit={this.onSubmit} autoComplete="new-password">
+          <Field>
+            <Input
+              className="mb-half"
+              name="name"
+              onChange={this.onChange}
+              placeholder="Name"
+              value={this.state.name}
+            />
 
-          <Textarea
-            className="mb-half"
-            name="description"
-            onChange={this.onChange}
-            placeholder="About Yourself"
-            rows={10}
-            onKeyDown={this.onEnterPress}
-            value={this.state.description}
-            color={valid ? null : 'danger'}
+            <Textarea
+              className="mb-half"
+              name="description"
+              onChange={this.onChange}
+              placeholder="About Yourself"
+              rows={10}
+              onKeyDown={this.onEnterPress}
+              value={this.state.description}
+              color={valid ? null : 'danger'}
+            />
+            {
+              !valid && <Help color="danger">Field can not be empty!</Help>
+            }
+            <div className="user-intro-form__location-wrapper">
+              <Input
+                className="mb-half"
+                name="area"
+                onChange={this.onCityChange}
+                placeholder="Location"
+                value={this.state.area}
+              />
+              <Dropdown
+                ref={(dropdown) => this.dropdown = dropdown }
+                value={this.state.selected}
+                color="info"
+                onChange={this.onDropdownChange}
+                >
+                {
+                  _.map(searchResults, (city) => {
+                    return (
+                      <Dropdown.Item
+                        onMouseEnter={() => this.setState({ hovered: city })}
+                        value={city}
+                        style={this.state.hovered === city ? { background: '#E0E3DA' } : {} }
+                        >
+                        {city}
+                      </Dropdown.Item>
+                    )
+                  })
+                }
+              </Dropdown>
+            </div>
+            <Input
+              name="websiteUrl"
+              onChange={this.onChange}
+              placeholder="Website"
+              value={this.state.websiteUrl}
+            />
+          </Field>
+          <SubmitFooter
+            onCancel={this.onCancel}
+            onSubmit={this.onSubmit}
           />
-          {
-            !valid && <Help color="danger">Field can not be empty!</Help>
-          }
-          <Input
-            className="mb-half"
-            name="city"
-            onChange={this.onCityChange}
-            placeholder="Location"
-            value={this.state.city}
-          />
-          <Input
-            name="websiteUrl"
-            onChange={this.onChange}
-            placeholder="Website"
-            value={this.state.websiteUrl}
-          />
-        </Field>
-        <SubmitFooter
-          onCancel={this.onCancel}
-          onSubmit={this.onSubmit}
-        />
-      </form>
+        </form>
+      </React.Fragment>
     )
   }
 }
