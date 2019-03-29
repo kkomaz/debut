4fa19640ac6959cbdb8ad2d848774e874bf34901
Alex Lee <alex@uniteus.com>
2019-03-29 16:18:13 -0400

    More clean-up

--- src/components/User/UserCard.jsx
+++ src/components/User/UserCard.jsx
@@ -16,9 +16,24 @@ class UserCard extends Component {
 
   render() {
     const { user, defaultImgUrl } = this.props
+
     return (
       <Card className="user-card__card">
-        <Card.Content className="user-card__card-banner">
+        <Card.Content className="user-card__card-banner" style={{ color: 'white'}}>
+          <div style={{ paddingLeft: '80px'}}>
+            <p
+              className="user-card__name"
+              onClick={this.navigateTo}
+            >
+              {_.get(user, 'profile.name', user.username)}
+            </p>
+            <p
+              className="user-card__username small"
+              onClick={this.navigateTo}
+            >
+              {user.username}
+            </p>
+          </div>
         </Card.Content>
         <Media className="user-card__media">
           <Media.Item renderAs="figure" position="left">
@@ -26,13 +41,10 @@ class UserCard extends Component {
               className="user-card__user-image"
               onClick={this.navigateTo}
               renderAs="p"
-              size={64}
-              alt="64x64"
+              size={80}
+              alt="80x80"
               src={_.get(user, 'profileImgUrl', defaultImgUrl)}
             />
-          </Media.Item>
-          <Media.Item className="user-card__user-identification">
-            <p onClick={this.navigateTo} className="user-card__name" size={6}>{`@${user.username}`}</p>
           </Media.Item>
         </Media>
         <Card.Content className="user-card__content">
