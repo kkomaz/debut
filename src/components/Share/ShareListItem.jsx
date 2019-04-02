311b74a0d458d48301356307fd9b8e60d600f17d
Alex Lee <alex@uniteus.com>
2019-04-02 17:59:03 -0400

    archive paginated comnets

--- src/components/Share/ShareListItem.jsx
+++ src/components/Share/ShareListItem.jsx
@@ -73,52 +73,6 @@ class ShareListItem extends Component {
     })
   }
 
-  /**
-   * This could use some refactoring <3
-  */
-  // renderShareCommentInfo = () => {
-  //   const { share } = this.props
-  //   const { commentView } = this.state
-  //
-  //   const result = (
-  //     share.commentCount && share.commentCount !== _.get(share, 'comments.length', 0) &&
-  //     <p
-  //       onClick={this.fetchShareComments}
-  //       className="small share-list-item__view-more-comments"
-  //       >
-  //       View Comments
-  //     </p>
-  //   )
-  //
-  //   if (result === 0) {
-  //     return null
-  //   }
-  //
-  //   if (_.isEqual(result, false)) {
-  //     if (commentView) {
-  //       return (
-  //         <p
-  //           onClick={this.hideCommentView}
-  //           className="small share-list-item__hide-comments"
-  //           >
-  //           Hide comments
-  //         </p>
-  //       )
-  //     } else {
-  //       return (
-  //         <p
-  //           onClick={this.showCommentView}
-  //           className="small share-list-item__view-more-comments"
-  //           >
-  //           View comments
-  //         </p>
-  //       )
-  //     }
-  //   }
-  //
-  //   return result
-  // }
-
   goToUserProfile = () => {
     const { history, username } = this.props
 
@@ -216,14 +170,6 @@ class ShareListItem extends Component {
         />
       </Popover>
     )
-  }
-
-  showCommentView = () => {
-    return this.setState({ commentView: true })
-  }
-
-  hideCommentView = () => {
-    return this.setState({ commentView: false })
   }
 
   toggleCommentView = () => {
