aa86fd186bdadbcb42d4d5f6a8fb766e1ed76db0
Alex Lee <alex@uniteus.com>
2019-04-07 19:32:17 -0400

    Clean uup css

--- src/pages/admin/notifications-comments/NotificationComments.jsx
+++ src/pages/admin/notifications-comments/NotificationComments.jsx
@@ -10,9 +10,20 @@ import { UserContext } from 'components/User/UserProvider'
 import { CSSTransitionGroup } from 'react-transition-group'
 import PropTypes from 'prop-types'
 import { css } from '@emotion/core'
+import moment from 'moment'
 
 // Action Imports
 import { requestAdminComments } from 'actions/comment'
+
+const formatDate = (input) => {
+  const postedDate = moment(input).fromNow()
+  const postedDateArray = postedDate.split(' ')
+
+  if (_.includes(postedDateArray, 'day') || _.includes(postedDateArray, 'days')) {
+    return moment(input).utc().format("MMM DD")
+  }
+  return postedDate
+}
 
 class NotificationComments extends Component {
   static propTypes = {
@@ -54,7 +65,18 @@ class NotificationComments extends Component {
                 <Card key={comment._id} className="mb-one">
                   <Card.Content>
                     <Content>
-                      Hello World
+                      <div>
+                        <p className="share-list-item__username-date" onClick={this.goToUserProfile}>
+                          <strong className="share-list-item__username">{comment.creator}</strong> <span className="admin-username__date small">- {formatDate(comment.createdAt)}</span>
+                        </p>
+
+                        <p>
+                          Comment to {comment.parent_creator}
+                        </p>
+                      </div>
+                      <p>
+                        {comment.text}
+                      </p>
                     </Content>
                   </Card.Content>
                 </Card>
