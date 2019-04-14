c89e8522497566937e16dfd1e1238cecc1d0353c
Alex Lee <alex@uniteus.com>
2019-04-14 10:23:22 -0400

    Clear up editShtShares

--- src/sagas/share/editShare.js
+++ src/sagas/share/editShare.js
@@ -1,23 +1,11 @@
 import { put, call } from 'redux-saga/effects'
 import { EDIT_SHARE_SUCCESS, EDIT_SHARE_FAIL } from 'actions'
 import Share from 'model/share'
-import checkMentions from 'utils/mentions/checkMentions'
-import Mention from 'model/mention'
 
 const editShare = async (action) => {
   const { params, id } = action
-  // let mentions = []
   const share = await Share.findById(id)
   share.update(params)
-
-  // const mention = [...new Set(checkMentions(params.text))]
-  // const filteredMentions = _.filter(mention, (m) => m.substring(1).trim() !== username)
-  //
-  // const mentionsList = Mention.fetchList({ parent_id: id })
-  //
-  // for (let i = 0; i < mentionsList.length; i++) {
-  //
-  // }
   return share.save()
 }
 
